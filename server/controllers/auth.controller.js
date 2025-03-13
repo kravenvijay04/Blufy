889f0dd815b0fa159fcfd2a767a00c71bfacac58
kravenvijay04 <vijaykumarnr04@gmail.com>
2025-03-13 14:46:26 +0530

    authority controller file is added

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,96 @@
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+import generateToken from "../utils/generateToken.js";
+export const signup = async (req, res) => {
+    try {
+        const { username, fullName, email, password } = req.body;
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ error: "Invalid email Format" })
+        }
+
+        const existingEmail = await User.findOne({ email: email });
+        const existingUsername = await User.findOne({ username });
+
+        if (existingEmail || existingUsername) {
+            return res.status(400).json({ error: "Already Existing User or email" })
+        }
+        if (password.length < 6) {
+            return res.status(400).json({ error: "Password must have atleast 6 char length" })
+        }
+
+        //hashing the password
+        //123456 = cn39f3yffj2fu2yf2
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            username,
+            fullName,
+            email,
+            password: hashedPassword
+        })
+
+        if (newUser) {
+            generateToken(newUser._id,res)
+            await newUser.save();
+            res.status(200).json({
+                _id: newUser._id,
+                userame: newUser.username,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                followers: newUser.followers,
+                following: newUser.following,
+                profileImg: newUser.profileImg,
+                coverImg: newUser.coverImg,
+                bio: newUser.bio,
+                link: newUser.link
+
+            })
+        }
+        else {
+            res.status(400).json({ error: "Invalid User Data" });
+        }
+
+    }
+    catch (error) {
+        console.log(`Error in signup controller : ${error}`)
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+};
+
+export const login = async (req, res) => {
+    try{
+        const{username,password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordCorrect = await bcrypt.compare(password,user?.password || "");
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({error:"Invalid username or password"})
+        }
+        generateToken(user._id,res);
+        
+        res.status(200).json({
+            _id: user._id,
+            userame: user.username,
+            fullName: user.fullName,
+            email: user.email,
+            followers: user.followers,
+            following: user.following,
+            profileImg: user.profileImg,
+            coverImg: user.cosverImg,
+            bio: user.bio,
+            link: user.link
+        })
+    }
+    catch(error){
+        console.log(`Error in login controller : ${error}`)
+        res.status(500).json({error:"Internal Server Error"});
+    }
+};
+
+export const logout = (req, res) => {
+    res.send("logout");
+};
