29b158b7723d44d1cd30ab2279ea77c85a81f264
kravenvijay04 <vijaykumarnr04@gmail.com>
2025-03-14 12:33:23 +0530

    changes in server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoute from "./routes/auth.route.js";
+import connectDB from "./db/connectDB.js"
 dotenv.config();
 const web = express();
 const PORT = process.env.PORT
@@ -10,8 +11,10 @@ web.get("/", (req, res) => {
     res.send("Hello World");
 })
 
-web.use("/api/auth",authRoute);
+web.use(express.json());
+web.use("/api/auth", authRoute);
 
-web.listen(PORT , () => {
-    console.log(`Server is running on ${PORT}`)
+web.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+    connectDB();
 })
