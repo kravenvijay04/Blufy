8e1c9641383a586f37ce3963fe924eba74d17a31
kravenvijay04 <vijaykumarnr04@gmail.com>
2025-06-28 09:18:18 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser"
+import cloudinary from "cloudinary"
 import authRoute from "./routes/auth.route.js";
+import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js";
 import connectDB from "./db/connectDB.js"
+
 dotenv.config();
+
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_sec: process.env.CLOUDINARY_API_SECRET_KEY
+})
 const web = express();
 const PORT = process.env.PORT
 
@@ -12,7 +22,10 @@ web.get("/", (req, res) => {
 })
 
 web.use(express.json());
+web.use(cookieParser());
 web.use("/api/auth", authRoute);
+web.use("/api/users", userRoute);
+web.use("/api/posts",postRoute);
 
 web.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
