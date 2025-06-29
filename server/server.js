afc26abe3cea1d3ade5c63900a7d99ede7067cb6
kravenvijay04 <vijaykumarnr04@gmail.com>
2025-06-29 21:00:43 +0530

    server.js is updated

--- server/server.js
+++ server/server.js
@@ -6,11 +6,12 @@ import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
 import connectDB from "./db/connectDB.js"
+import notificationRoute from "./routes/notification.route.js"
 
 dotenv.config();
 
 cloudinary.config({
-    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_sec: process.env.CLOUDINARY_API_SECRET_KEY
 })
@@ -25,7 +26,8 @@ web.use(express.json());
 web.use(cookieParser());
 web.use("/api/auth", authRoute);
 web.use("/api/users", userRoute);
-web.use("/api/posts",postRoute);
+web.use("/api/posts", postRoute);
+web.use("/api/notifications", notificationRoute)
 
 web.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
