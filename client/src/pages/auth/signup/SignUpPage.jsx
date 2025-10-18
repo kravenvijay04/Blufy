a6666b721d70e80edfcae08099a03ed852045a5d
kravenvijay04 <vijaykumarnr04@gmail.com>
2025-10-18 17:38:59 +0530

    signup logo is added

--- client/src/pages/auth/signup/SignUpPage.jsx
+++ client/src/pages/auth/signup/SignUpPage.jsx
@@ -59,16 +59,16 @@ const SignUpPage = () => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
 
-
-
     return (
         <div className='max-w-screen-xl mx-auto flex h-screen px-10'>
-            <div className='flex-1 hidden lg:flex items-center  justify-center'>
-                <XSvg className=' lg:w-2/3 fill-white' />
+            <div className='flex-1 hidden lg:flex items-center justify-center'>
+                <XSvg className='lg:w-1/3 fill-white' />
             </div>
             <div className='flex-1 flex flex-col justify-center items-center'>
                 <form className='lg:w-2/3  mx-auto md:mx-20 flex gap-4 flex-col' onSubmit={handleSubmit}>
-                    <XSvg className='w-24 lg:hidden fill-white' />
+                    <div className='flex lg:hidden justify-center'>
+                        <XSvg className='w-24 fill-white' />
+                    </div>
                     <h1 className='text-4xl font-extrabold text-white'>Join today.</h1>
                     <label className='input input-bordered rounded flex items-center gap-2'>
                         <MdOutlineMail />
