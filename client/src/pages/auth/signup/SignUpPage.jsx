a43c4106f2a4c4bfc7e6622e711357c42eab8d35
kravenvijay04 <vijaykumarnr04@gmail.com>
2025-10-18 18:24:28 +0530

    logo size changes

--- client/src/pages/auth/signup/SignUpPage.jsx
+++ client/src/pages/auth/signup/SignUpPage.jsx
@@ -62,7 +62,13 @@ const SignUpPage = () => {
     return (
         <div className='max-w-screen-xl mx-auto flex h-screen px-10'>
             <div className='flex-1 hidden lg:flex items-center justify-center'>
-                <XSvg className='lg:w-1/3 fill-white' />
+                <img
+                    src="logo.png"
+                    width="330"
+                    height="330"
+                    style={{ paddingRight: '0px' }}
+                />
+
             </div>
             <div className='flex-1 flex flex-col justify-center items-center'>
                 <form className='lg:w-2/3  mx-auto md:mx-20 flex gap-4 flex-col' onSubmit={handleSubmit}>
